3281eccc30e63baa41e769658586a8f141e903f6
Azeeng <124342543+Ohazing@users.noreply.github.com>
2023-10-18 21:59:42 +0700

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -34,7 +34,7 @@ contactForm.addEventListener("submit", function (e) {
   })
     .then(() => {
       loader.style.display = "none";
-      window.location.href = "/thankyou.html";
+      window.location.href = "thankyou.html";
     })
     .catch((e) => alert("Error occured"));
 });
